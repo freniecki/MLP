58029ad056290ebb750153a8b58165e6d2ddaec2
freniecki <247773@edu.p.lodz.pl>
2024-06-04 13:35:22 +0200

    error(epoch) plot

---
+++ app/src/main/java/pl/firaanki/Plot.java
@@ -0,0 +1,40 @@
+package pl.firaanki;
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.category.DefaultCategoryDataset;
+import javax.swing.JFrame;
+
+public class Plot extends JFrame {
+    public Plot(String title, double[] errors) {
+        super(title);
+
+        // Create dataset
+        DefaultCategoryDataset dataset = createDataset(errors);
+
+        // Create chart
+        JFreeChart chart = ChartFactory.createLineChart(
+                "Error Plot",
+                "Index",
+                "Error Value",
+                dataset,
+                PlotOrientation.VERTICAL,
+                true, true, false);
+
+        // Create Panel
+        ChartPanel panel = new ChartPanel(chart);
+        setContentPane(panel);
+    }
+
+    private DefaultCategoryDataset createDataset(double[] errors) {
+        DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+
+        for (int i = 0; i < errors.length; i++) {
+            dataset.addValue(errors[i], "Errors", Integer.toString(i));
+        }
+
+        return dataset;
+    }
+}
